83b9944e6d3e9c44a3f753d5c7e0c4d4e5039579
Tamar Shouker <tamsho@gmail.com>
2014-04-03 21:34:21 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BShulim::Application.config.secret_key_base = 'a2d3d10dab1a235c79a3c6fde2fd2b76c2f5dc0d1e071dabe500c87f004d73ac4909ba50649979b578b8df70e250b04e96b9ce517f09fd68c0aa5c83044240bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BShulim::Application.config.session_store :cookie_store, key: '_b-shulim_session'
