44e296d1033b658db772b9840b06111ebef6308c
Tamar Shouker <tamsho@gmail.com>
2014-04-05 11:48:34 +0300

    some fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+	def link_to_image(image_path, target_link, image_options, link_options={})
+	  link_to(image_tag(image_path, image_options), target_link, link_options)
+	end
 end
